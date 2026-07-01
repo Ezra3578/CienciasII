ce8b8f84097b11f57ebdaf848e899e6669c3715f
Ana <111031121+AnaTuPatrona@users.noreply.github.com>
2026-07-01 13:21:49 +0000

    creacion interface para arboles de expansion

---
+++ Dijkstra/ArbolExpansionMinima.java
@@ -0,0 +1,5 @@
+public interface ArbolExpansionMinima{
+    public void calcularExpansionMinima(); //se le pasaría la lista de adjacencia en el constructor, por eso este no acepta parametros
+    public String getListaAristasExpansion();
+
+}

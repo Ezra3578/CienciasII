505b2516ac200854977871edb01eea71e63f0a32
Ana <111031121+AnaTuPatrona@users.noreply.github.com>
2026-07-01 13:23:54 +0000

    comentario pendejo

--- Dijkstra/ArbolExpansionMinima.java
+++ Dijkstra/ArbolExpansionMinima.java
@@ -1,5 +1,5 @@
 public interface ArbolExpansionMinima{
     public void calcularExpansionMinima(); //se le pasaría la lista de adjacencia en el constructor, por eso este no acepta parametros
-    public String getListaAristasExpansion();
+    public String getListaAristasExpansion(); //favor mostrar como lista, como matriz se vería raro
 
 }

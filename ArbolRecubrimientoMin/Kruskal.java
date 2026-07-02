5e490543db2c121ec87c121e1b5efe73fa925d97
Ana <111031121+AnaTuPatrona@users.noreply.github.com>
2026-07-02 17:11:10 +0000

    base del archivo de kruskal

---
+++ ArbolRecubrimientoMin/Kruskal.java
@@ -0,0 +1,24 @@
+public class Kruskal implements AlgoritmoMSP{
+
+    private ListaAdyacencia grafo;
+
+    public Kruskal(ListaAdyacencia grafo){
+
+    }
+
+    @Override
+    public void encontrarMSP(String nodo_inicial){
+        System.out.println("hola");
+    }
+
+    @Override
+    public String getCamino(String nodo_inicial){
+        return "sajfns";
+    }
+
+    @Override
+    public int getPasos(){
+        return 0;
+    }
+
+}
